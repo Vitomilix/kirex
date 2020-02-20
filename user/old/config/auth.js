229b8b0ce56dca6435298aecb1a7000a30b48188
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-02-20 20:56:49 +0200

    removed old files

--- user/old/config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  // if not authenticated, redirect to login page
-  if (!req.isAuthenticated()) {
-    req.flash('reminder', 'Hold on, where are you going. Log in first')
-    return res.redirect('/users/login')
-  }
-  next()
-}
