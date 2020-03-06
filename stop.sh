cc0d43d91e1c61135dedbf368c0f5c6970c4f0d0
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-03-06 10:32:01 +0100

    updated script

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
-#stop kire
+#stop kirex
 echo stopping services
 echo stopping user
-kill -9 $(lsof -t -i:3000)
+fuser -n tcp -k 3000
 echo stopping admin
-kill -9 $(lsof -t -i:3001)
+fuser -n tcp -k 3001
 echo all services stopped
