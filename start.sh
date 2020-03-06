e8b412c6144a46a254d6039640ab80e8c87e2420
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-03-06 10:25:21 +0100

    Added start and stop script

---
+++ start.sh
@@ -0,0 +1,8 @@
+echo starting user
+cd user
+node app.js &
+cd ..
+echo starting admin
+cd admin
+node app.js &
+echo started all services
---
+++ stop.sh
@@ -0,0 +1,6 @@
+echo stopping services
+echo stopping user
+kill -9 $(lsof -t -i:3000)
+echo stopping admin
+kill -9 $(lsof -t -i:3001)
+echo all services stopped
