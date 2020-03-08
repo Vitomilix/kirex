944586f1687c6d2374fb699134d6a1ca2e53bedc
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-03-08 17:13:59 +0100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 # SETUP
-#!/bin/bash
-sh ./pre-setup
+
+                                                  
+echo -e "\e[1;32m _|    _|  _|_|_|  _|_|_|    _|_|_|_|  _|      _| \e[0m" 
+echo -e "\e[1;32m _|  _|      _|    _|    _|  _|          _|  _|   \e[0m" 
+echo -e "\e[1;32m _|_|        _|    _|_|_|    _|_|_|        _|     \e[0m" 
+echo -e "\e[1;32m _|  _|      _|    _|    _|  _|          _|  _|    \e[0m"
+echo -e "\e[1;32m _|    _|  _|_|_|  _|    _|  _|_|_|_|  _|      _| \e[0m"
+
+
+
+
+echo Welcome to KirEx 1.0.5
+
+echo -e "\e[1;31m Copyright © Vitomilix 2020 All rights reserved\e[0m" 
+
+echo Simply Answer the prompts on the screen to get KirEx Running
+
 
 
 echo getting things ready...
