fd70493cb11747543cdcc635f024e8a459161f1a
Andreas Krueger <andreas.krueger@student.tugraz.at>
2020-03-08 16:58:33 +0100

    updated scripts

--- pre-setup.sh
+++ pre-setup.sh
@@ -7,10 +7,10 @@ echo -e "\e[1;32m _|  _|      _|    _|    _|  _|          _|  _|    \e[0m"
 echo -e "\e[1;32m _|    _|  _|_|_|  _|    _|  _|_|_|_|  _|      _| \e[0m"
 
 
-echo -e \n\n\n
+printf \n \n
 
 echo Welcome to KirEx 1.0.5
 
-echo echo -e "\e[1;31m Copyright © Vitomilix 2020 All rights reserved\e[0m" 
+echo -e "\e[1;31m Copyright © Vitomilix 2020 All rights reserved\e[0m" 
 
 echo Simply Answer the prompts on the screen to get KirEx Running
